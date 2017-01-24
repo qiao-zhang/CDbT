ac5cd79cf1fe6796a4e14c0686721fab6a484960
Qiao Zhang <qiao.zhang.2015@gmail.com>
2017-01-24 11:35:39 -0500

    create CoreDataStack

---
+++ Dog Walk/CoreDataStack.swift
@@ -0,0 +1,41 @@
+//
+//  CoreDataStack.swift
+//  CDbT
+//
+//  Created by Qiao Zhang on 1/24/17.
+//  Copyright © 2017 Qiao Zhang. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class CoreDataStack {
+  private let modelName: String
+  init(modelName: String) {
+    self.modelName = modelName
+  }
+  
+  private lazy var storeContainer: NSPersistentContainer = {
+    let container = NSPersistentContainer(name: self.modelName)
+    container.loadPersistentStores { _, error in
+      if let error = error as NSError? {
+        print("Unresolved error \(error), \(error.userInfo)")
+      }
+    }
+    return container
+  }()
+  
+  lazy var managedContext: NSManagedObjectContext = {
+    return self.storeContainer.viewContext
+  }()
+  
+  func saveContext() {
+    guard managedContext.hasChanges else { return }
+    do {
+      try managedContext.save()
+    } catch {
+      let nsError = error as NSError
+      print("Unresolved error \(nsError), \(nsError.userInfo)")
+    }
+  }
+}
