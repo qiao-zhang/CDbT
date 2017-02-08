1332637d1e63bc82c08619182b9af7c1a54f88d3
Qiao Zhang <qiao.zhang.2015@gmail.com>
2017-02-08 13:10:58 -0500

    add v3 data model and make relevant changes in code

---
+++ UnCloudNotes/Attachment.swift
@@ -0,0 +1,16 @@
+//
+//  Attachment.swift
+//  CDbT
+//
+//  Created by Qiao Zhang on 2/8/17.
+//  Copyright © 2017 Qiao Zhang. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class Attachment: NSManagedObject {
+  @NSManaged var dateCreated: Date
+  @NSManaged var image: UIImage?
+  @NSManaged var note: Note?
+}
--- UnCloudNotes/CreateNoteViewController.swift
+++ UnCloudNotes/CreateNoteViewController.swift
@@ -46,7 +46,7 @@ class CreateNoteViewController: UIViewController, UsesCoreDataObjects {
       titleField.becomeFirstResponder()
       return
     }    
-    attachedPhoto.image = note?.image
+    attachedPhoto.image = image
     view.endEditing(true)
   }
   
